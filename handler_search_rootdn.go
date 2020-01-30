d6e6e029ce47d1f799d283249da6b2f7db814ca1
Hiroyuki Wada <wadahiro@gmail.com>
2020-01-30 18:56:49 +0900

    fix: add missing new source

---
+++ handler_search_rootdn.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/openstandia/goldap/message"
+	ldap "github.com/openstandia/ldapserver"
+)
+
+func handleSearchRootDN(s *Server, w ldap.ResponseWriter, m *ldap.Message) {
+	r := m.GetSearchRequest()
+
+	log.Printf("info: handleSearchRootDN")
+	log.Printf("info: Request BaseDn=%s", r.BaseObject())
+	log.Printf("info: Request Filter=%s", r.Filter())
+	log.Printf("info: Request FilterString=%s", r.FilterString())
+	log.Printf("info: Request Attributes=%s", r.Attributes())
+	log.Printf("info: Request TimeLimit=%d", r.TimeLimit().Int())
+
+	e := ldap.NewSearchResultEntry(s.GetRootDN().DNOrigStr() + "," + s.GetSuffix())
+
+	uuid, _ := uuid.NewRandom()
+
+	// Define all attributes
+	searchEntry := NewSearchEntry(nil, map[string][]string{
+		"objectClass":           []string{"simpleSecurityObject", "organizationalRole"},
+		"structuralObjectClass": []string{"organizationalRole"},
+		"cn":                    []string{s.GetRootDN().RDN()["cn"]},
+		"description":           []string{"LDAP administrator"},
+		// OperationalAttributes
+		"entryUUID":         []string{uuid.String()},
+		"creatorsName":      []string{s.GetRootDN().DNOrigStr()},
+		"modifiersName":     []string{s.GetRootDN().DNOrigStr()},
+		"entryDN":           []string{s.GetRootDN().DNOrigStr()},
+		"subschemaSubentry": []string{"cn=Subschema"},
+		"hasSubordinates":   []string{"FALSE"},
+	})
+
+	// TODO: implement filter
+
+	sentAttrs := map[string]struct{}{}
+
+	if isAllAttributesRequested(r) {
+		for k, v := range searchEntry.GetAttrsOrigWithoutOperationalAttrs() {
+			log.Printf("- Attribute %s: %#v", k, v)
+
+			av := make([]message.AttributeValue, len(v))
+			for i, vv := range v {
+				av[i] = message.AttributeValue(vv)
+			}
+			e.AddAttribute(message.AttributeDescription(k), av...)
+
+			sentAttrs[k] = struct{}{}
+		}
+	}
+
+	for _, attr := range r.Attributes() {
+		a := string(attr)
+
+		log.Printf("Requested attr: %s", a)
+
+		if a != "+" {
+			k, values, ok := searchEntry.GetAttrOrig(a)
+			if !ok {
+				log.Printf("No schema for requested attr, ignore. attr: %s", a)
+				continue
+			}
+
+			if _, ok := sentAttrs[k]; ok {
+				log.Printf("Already sent, ignore. attr: %s", a)
+				continue
+			}
+
+			log.Printf("- Attribute %s=%#v", a, values)
+
+			av := make([]message.AttributeValue, len(values))
+			for i, vv := range values {
+				av[i] = message.AttributeValue(vv)
+			}
+			e.AddAttribute(message.AttributeDescription(k), av...)
+
+			sentAttrs[k] = struct{}{}
+		}
+	}
+
+	if isOperationalAttributesRequested(r) {
+		for k, v := range searchEntry.GetOperationalAttrsOrig() {
+			if _, ok := sentAttrs[k]; !ok {
+				for _, vv := range v {
+					e.AddAttribute(message.AttributeDescription(k), message.AttributeValue(vv))
+				}
+			}
+		}
+	}
+
+	// e.AddAttribute("supportedExtension", "1.3.6.1.4.1.1466.20036", "1.3.6.1.4.1.4203.1.11.1", "1.3.6.1.4.1.18060.0.1.5", "1.3.6.1.4.1.18060.0.1.3", "1.3.6.1.4.1.1466.20037")
+	w.Write(e)
+
+	res := ldap.NewSearchResultDoneResponse(ldap.LDAPResultSuccess)
+	w.Write(res)
+}
